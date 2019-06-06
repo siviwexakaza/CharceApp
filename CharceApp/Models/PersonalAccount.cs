ae904f1293b2b447d25463ff60d31ca3b406a77e
siviwe <xboyfan01>
2019-06-06 01:20:34 -0700

    Add User model

---
+++ CharceApp/Models/PersonalAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CharceApp.Models
+{
+    public class PersonalAccount
+    {
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Names { get; set; }
+        public string Surname { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+    }
+}
